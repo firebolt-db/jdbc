b4cd89f13aebcf0901769ca416d142e1ff20a9fa
Aymeric <aymeric@firebolt.io>
2022-06-10 15:37:12 +0200

    Fix method to clean query and add unquoted wort count

--- src/main/java/io/firebolt/QueryUtil.java
+++ src/main/java/io/firebolt/QueryUtil.java
@@ -4,6 +4,7 @@ import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RegExUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.Optional;
@@ -22,12 +23,12 @@ public class QueryUtil {
       new String[] {"show", "select", "describe", "exists", "explain"};
 
   public static boolean isSelect(String sql) {
-    String cleanQuery = removeCommentsAndTrimQuery(sql);
+    String cleanQuery = cleanQuery(sql);
     return StringUtils.startsWithAny(cleanQuery.toLowerCase(), SELECT_KEYWORDS);
   }
 
   public Optional<Pair<String, String>> extractAdditionalProperties(String sql) {
-    String cleanQuery = removeCommentsAndTrimQuery(sql);
+    String cleanQuery = cleanQuery(sql);
     if (cleanQuery.toLowerCase().startsWith(SET_PREFIX)) {
       return extractPropertyPair(sql, cleanQuery);
     } else {
@@ -35,9 +36,59 @@ public class QueryUtil {
     }
   }
 
-  public String removeCommentsAndTrimQuery(String sql) {
-    return RegExUtils.replaceAll(sql, ALL_COMMENTS_REGEX, "")
-        .trim(); // Replace substrings that starts with -- and ends with \n OR /* and ends with */
+  public String cleanQuery(String sql) {
+    return cleanQueryAndCountUnquotedWordOccurrences(sql, null).getLeft();
+  }
+
+  public Pair<String, Integer> cleanQueryAndCountUnquotedWordOccurrences(String sql, String searchedWord) {
+    int searchedWordCount = 0;
+    StringBuilder result = new StringBuilder();
+    sql = sql.trim();
+    int currentIndex = 0;
+    char currentChar = sql.charAt(currentIndex);
+    boolean isCurrentSubstringBetweenQuotes = currentChar == 39; // 39 is the ASCII for '
+    int substringStart = 0;
+    while (currentIndex < sql.length() - 1) {
+      currentIndex++;
+      currentChar = sql.charAt(currentIndex);
+      if (39 == currentChar) {
+        if (isCurrentSubstringBetweenQuotes) {
+          String subString = StringUtils.substring(sql, substringStart, currentIndex + 1);
+          result.append(subString);
+        } else {
+          Pair<String, Integer> cleanSubstring =
+              cleanQueryPartAndCountWordOccurrences(
+                  sql, substringStart, currentIndex + 1, searchedWord);
+          result.append(cleanSubstring.getLeft());
+          searchedWordCount += cleanSubstring.getRight();
+        }
+        substringStart = currentIndex + 1;
+        isCurrentSubstringBetweenQuotes = !isCurrentSubstringBetweenQuotes;
+      }
+    }
+    if (substringStart < sql.length()) {
+      Pair<String, Integer> cleanSubstring =
+          cleanQueryPartAndCountWordOccurrences(
+              sql, substringStart, sql.length() + 1, searchedWord);
+      result.append(cleanSubstring.getLeft());
+      searchedWordCount += cleanSubstring.getRight();
+    }
+    return new ImmutablePair<>(result.toString().trim(), searchedWordCount);
+  }
+
+  private Pair<String, Integer> cleanQueryPartAndCountWordOccurrences(
+      String sql, int substringStart, int substringEnd, String searchedWord) {
+    String subString = StringUtils.substring(sql, substringStart, substringEnd);
+    int count = 0;
+    String unquotedWord = removeCommentsFromSubstring(subString);
+    if (searchedWord != null) {
+      count = StringUtils.countMatches(unquotedWord, searchedWord);
+    }
+    return new ImmutablePair<>(unquotedWord, count);
+  }
+
+  private String removeCommentsFromSubstring(String subString) {
+    return RegExUtils.replaceAll(subString, ALL_COMMENTS_REGEX, "");
   }
 
   private Optional<Pair<String, String>> extractPropertyPair(String sql, String query) {
@@ -72,7 +123,7 @@ public class QueryUtil {
   private static Optional<String> extractDBAndTableNameFromSelect(String sql) {
     if (isSelect(sql)) {
       log.debug("Extracting DB and Table name for SELECT: {}", sql);
-      String cleanQuery = removeCommentsAndTrimQuery(sql);
+      String cleanQuery = cleanQuery(sql);
       String withoutQuotes = StringUtils.replace(cleanQuery, "'", "").trim();
       if (StringUtils.startsWithIgnoreCase(withoutQuotes, "select")) {
         int fromIndex = StringUtils.indexOfIgnoreCase(withoutQuotes, "from");
@@ -85,10 +136,13 @@ public class QueryUtil {
         return Optional.of("information_schema.tables");
       }
       if (StringUtils.startsWithIgnoreCase(withoutQuotes, "SHOW")) {
-        return Optional.of("information_schema.unknown"); // Depends on the information requested - so putting unknown as a placeholder
+        return Optional.of(
+            "information_schema.unknown"); // Depends on the information requested - so putting
+        // unknown as a placeholder
       }
     }
-    log.debug("Could not find table name for query {}. This may happen when there is no table.", sql);
+    log.debug(
+        "Could not find table name for query {}. This may happen when there is no table.", sql);
     return Optional.empty();
   }
 }
