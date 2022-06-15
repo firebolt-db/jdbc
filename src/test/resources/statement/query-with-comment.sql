e748564124a83ac8f1406afea8228e5e85aad434
Aymeric <aymeric@firebolt.io>
2022-06-15 10:30:44 +0100

    Add mapping for infinity and fix query character count when characters are in comments

--- src/test/resources/statement/query-with-comment.sql
+++ src/test/resources/statement/query-with-comment.sql
@@ -2,8 +2,8 @@
 select query.server_date_utc,
        query.login,
        CAST(CASE
-                WHEN bid_func = 'set_referrer' THEN '-2/* hey*/'/* comment */
-                WHEN LENGTH(partner_id) > 0 THEN partner_id
+                WHEN bid_func = 'set_referrer' THEN '-2/* hey?*/'/* comment ?*/
+                WHEN LENGTH(partner_id) > ? THEN partner_id
                 ELSE alternate_partner_id END as float) AS partner_id,
 -- -- Comment
 -- Comment
