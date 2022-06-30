85b621a005252f5edb47c34639b0f55f65d29b9e
Aymeric <aymeric@firebolt.io>
2022-06-30 13:51:05 +0100

    Add com.firebolt.FireboltDriver to make the driver backward compatible

---
+++ src/main/java/com/firebolt/FireboltDriver.java
@@ -0,0 +1,7 @@
+package com.firebolt;
+
+/**
+ * Class needed to make the driver backward compatible.
+ * Once all the changes released, io.firebolt package will be replaced by com.firebolt
+ */
+public class FireboltDriver extends io.firebolt.FireboltDriver {}
