abfd4b2b8dc51f789a37c7b09f905d6e2b3212f1
Aymeric <aymeric@firebolt.io>
2022-12-05 16:56:29 +0000

    Remove export script

--- export_performance_test_result.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-while IFS="," read -r line
-do
-   echo "| $line| " >> "$GITHUB_STEP_SUMMARY"
-done < <(grep . result.csv | sed 1d | tail -n 15 | tr ',' '|') #Reads the last 15 lines except the header
