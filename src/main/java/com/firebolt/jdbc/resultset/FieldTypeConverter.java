7fd8ddca01dad02565823fb13261ea2167e214b2
Aymeric <aymeric@firebolt.io>
2023-02-13 16:38:49 +0000

    Improve variable name of the map containing the conversion functions

--- src/main/java/com/firebolt/jdbc/resultset/FieldTypeConverter.java
+++ src/main/java/com/firebolt/jdbc/resultset/FieldTypeConverter.java
@@ -20,77 +20,77 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class FieldTypeConverter {
 
-	private static final Map<Class<?>, CheckedTriFunction<String, BaseType, Column, ?>> javaClassToConvertFunction;
+	private static final Map<Class<?>, CheckedTriFunction<String, BaseType, Column, ?>> CLASS_TO_CONVERT_FUNCTION;
 
 	private static final String CONVERSION_NOT_SUPPORTED_EXCEPTION = "conversion to %s from %s not supported";
 
 	static {
-		javaClassToConvertFunction = new HashMap<>();
+		CLASS_TO_CONVERT_FUNCTION = new HashMap<>();
 
-		javaClassToConvertFunction.put(String.class, (value, columnType, column) -> {
+		CLASS_TO_CONVERT_FUNCTION.put(String.class, (value, columnType, column) -> {
 			verify(String.class, columnType, BaseType.STRING);
 			return BaseType.STRING.transform(value, column);
 		});
-		javaClassToConvertFunction.put(Integer.class, (value, columnType, column) -> {
+		CLASS_TO_CONVERT_FUNCTION.put(Integer.class, (value, columnType, column) -> {
 			verify(Integer.class, columnType, BaseType.INTEGER, BaseType.SHORT);
 			return BaseType.INTEGER.transform(value, column);
 		});
-		javaClassToConvertFunction.put(Long.class, (value, columnType, column) -> {
+		CLASS_TO_CONVERT_FUNCTION.put(Long.class, (value, columnType, column) -> {
 			verify(Long.class, columnType, BaseType.INTEGER, BaseType.SHORT, BaseType.LONG);
 			return BaseType.LONG.transform(value, column);
 		});
-		javaClassToConvertFunction.put(Double.class, (value, columnType, column) -> {
+		CLASS_TO_CONVERT_FUNCTION.put(Double.class, (value, columnType, column) -> {
 			verify(Double.class, columnType, BaseType.DOUBLE, BaseType.FLOAT);
 			return BaseType.DOUBLE.transform(value, column);
 		});
-		javaClassToConvertFunction.put(Boolean.class, (value, columnType, column) -> {
+		CLASS_TO_CONVERT_FUNCTION.put(Boolean.class, (value, columnType, column) -> {
 			verify(Boolean.class, columnType, BaseType.BOOLEAN);
 			return BaseType.BOOLEAN.transform(value, column);
 		});
-		javaClassToConvertFunction.put(Short.class, (value, columnType, column) -> {
+		CLASS_TO_CONVERT_FUNCTION.put(Short.class, (value, columnType, column) -> {
 			verify(Short.class, columnType, BaseType.SHORT);
 			return BaseType.SHORT.transform(value, column);
 		});
-		javaClassToConvertFunction.put(BigInteger.class, (value, columnType, column) -> {
+		CLASS_TO_CONVERT_FUNCTION.put(BigInteger.class, (value, columnType, column) -> {
 			verify(BigInteger.class, columnType, BaseType.INTEGER, BaseType.SHORT, BaseType.LONG, BaseType.BIGINT);
 			return BaseType.BIGINT.transform(value, column);
 		});
-		javaClassToConvertFunction.put(Float.class, (value, columnType, column) -> {
+		CLASS_TO_CONVERT_FUNCTION.put(Float.class, (value, columnType, column) -> {
 			verify(Float.class, columnType, BaseType.FLOAT);
 			return BaseType.FLOAT.transform(value, column);
 		});
-		javaClassToConvertFunction.put(BigDecimal.class, (value, columnType, column) -> {
+		CLASS_TO_CONVERT_FUNCTION.put(BigDecimal.class, (value, columnType, column) -> {
 			verify(BigDecimal.class, columnType, BaseType.BIGINT, BaseType.DECIMAL, BaseType.INTEGER, BaseType.FLOAT,
 					BaseType.DOUBLE);
 			return BaseType.DECIMAL.transform(value, column);
 		});
-		javaClassToConvertFunction.put(Date.class, (value, columnType, column) -> {
+		CLASS_TO_CONVERT_FUNCTION.put(Date.class, (value, columnType, column) -> {
 			verify(Date.class, columnType, BaseType.DATE);
 			return BaseType.DATE.transform(value, column);
 		});
-		javaClassToConvertFunction.put(Time.class, (value, columnType, column) -> {
+		CLASS_TO_CONVERT_FUNCTION.put(Time.class, (value, columnType, column) -> {
 			verify(Time.class, columnType, BaseType.TIME);
 			return BaseType.TIME.transform(value, column);
 		});
-		javaClassToConvertFunction.put(Timestamp.class, (value, columnType, column) -> {
+		CLASS_TO_CONVERT_FUNCTION.put(Timestamp.class, (value, columnType, column) -> {
 			verify(Timestamp.class, columnType, BaseType.TIMESTAMP, BaseType.TIMESTAMP_WITH_TIMEZONE);
 			return BaseType.TIMESTAMP.transform(value, column);
 		});
-		javaClassToConvertFunction.put(Array.class, (value, columnType, column) -> {
+		CLASS_TO_CONVERT_FUNCTION.put(Array.class, (value, columnType, column) -> {
 			verify(Array.class, columnType, BaseType.ARRAY);
 			return BaseType.ARRAY.transform(value, column);
 		});
-		javaClassToConvertFunction.put(OffsetDateTime.class, (value, columnType, column) -> {
+		CLASS_TO_CONVERT_FUNCTION.put(OffsetDateTime.class, (value, columnType, column) -> {
 			verify(OffsetDateTime.class, columnType, BaseType.DATE, BaseType.TIMESTAMP,
 					BaseType.TIMESTAMP_WITH_TIMEZONE);
 			Timestamp ts = BaseType.TIMESTAMP.transform(value, column);
 			return SqlDateUtil.transformFromTimestampToOffsetDateTime.apply(ts);
 		});
-		javaClassToConvertFunction.put(Object.class, (value, columnType, column) -> {
+		CLASS_TO_CONVERT_FUNCTION.put(Object.class, (value, columnType, column) -> {
 			verify(Object.class, columnType, BaseType.OBJECT, BaseType.NULL);
 			return BaseType.OBJECT.transform(value, column);
 		});
-		javaClassToConvertFunction.put(byte[].class, (value, columnType, column) -> {
+		CLASS_TO_CONVERT_FUNCTION.put(byte[].class, (value, columnType, column) -> {
 			if (columnType == BaseType.BYTEA) {
 				verify(byte[].class, columnType, BaseType.BYTEA);
 				return BaseType.BYTEA.transform(value, column);
@@ -110,10 +110,10 @@ public class FieldTypeConverter {
 	}
 
 	static <T> T convert(Class<T> type, String value, BaseType columnType, Column column) throws SQLException {
-		if (!javaClassToConvertFunction.containsKey(type)) {
+		if (!CLASS_TO_CONVERT_FUNCTION.containsKey(type)) {
 			throw new FireboltException(
 					String.format(CONVERSION_NOT_SUPPORTED_EXCEPTION, type.getName(), columnType.getType().getName()));
 		}
-		return (T) javaClassToConvertFunction.get(type).apply(value, columnType, column);
+		return (T) CLASS_TO_CONVERT_FUNCTION.get(type).apply(value, columnType, column);
 	}
 }
