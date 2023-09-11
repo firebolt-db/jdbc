bc3d99c9b5f4f530f1f69c909606624548662ba3
Alexander Radzin <alexander.radzin@firebolt.io>
2023-09-11 16:58:55 +0300

    bug: FIR-26375 throw exception if account  is not  specified (#276)

--- src/integrationTest/java/integration/tests/StatementTest.java
+++ src/integrationTest/java/integration/tests/StatementTest.java
@@ -15,7 +15,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static java.lang.String.format;
