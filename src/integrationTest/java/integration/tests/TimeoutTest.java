53913c6840ecb02a4ec28e8b040e74208408fa31
alexradzin <alexander.radzin@firebolt.io>
2023-12-19 11:49:16 +0200

    FIR-25160: improved long query for test

--- src/integrationTest/java/integration/tests/TimeoutTest.java
+++ src/integrationTest/java/integration/tests/TimeoutTest.java
@@ -9,6 +9,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
@@ -20,16 +21,13 @@ class TimeoutTest extends IntegrationTest {
 
 	@Test
 	@Timeout(value = 7, unit = TimeUnit.MINUTES)
+	@Tag("slow")
 	void shouldExecuteRequestWithoutTimeout() throws SQLException {
 		long startTime = System.nanoTime();
 		try (Connection con = this.createConnection(); Statement stmt = con.createStatement()) {
-			String numbers = IntStream.range(1, 10_000).boxed().map(String::valueOf).collect(Collectors.joining(","));
-			String query = String.format("WITH arr AS (SELECT [%s] as a)%nSELECT md5(md5(md5(md5(md5(md5(md5(to_string(a)))))))) FROM arr UNNEST(a)", numbers);
-			stmt.executeQuery(query);
-		} catch (Exception e) {
-			log.error("Error", e);
-			fail();
+			stmt.executeQuery("SELECT checksum(*) FROM generate_series(1, 100000000000)");
+		} finally {
+			log.info("Time elapsed: " + (System.nanoTime() - startTime) / 1_000_000_000 + " seconds");
 		}
-		log.info("Time elapsed: " + (System.nanoTime() - startTime) / 1_000_000_000 + " seconds");
 	}
 }
