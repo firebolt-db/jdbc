34fee515d4576da9d814af6f1322593a3b68454d
Alexander Radzin <alexander.radzin@firebolt.io>
2023-12-24 15:54:56 +0200

    special treatment of 404 status when accessing account (#309)
    Co-authored-by: Petro Tiurin <93913847+ptiurin@users.noreply.github.com>

--- src/test/java/com/firebolt/jdbc/client/gateway/FireboltGatewayUrlClientTest.java
+++ src/test/java/com/firebolt/jdbc/client/gateway/FireboltGatewayUrlClientTest.java
@@ -1,6 +1,7 @@
 package com.firebolt.jdbc.client.gateway;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.firebolt.jdbc.annotation.ExcludeFromJacocoGeneratedReport;
 import com.firebolt.jdbc.client.account.FireboltAccount;
 import com.firebolt.jdbc.client.account.FireboltAccountRetriever;
 import com.firebolt.jdbc.connection.FireboltConnection;
@@ -9,16 +10,46 @@ import okhttp3.Call;
 import okhttp3.OkHttpClient;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 
+import static java.lang.String.format;
+import static java.net.HttpURLConnection.HTTP_BAD_GATEWAY;
+import static java.net.HttpURLConnection.HTTP_BAD_METHOD;
+import static java.net.HttpURLConnection.HTTP_BAD_REQUEST;
+import static java.net.HttpURLConnection.HTTP_CLIENT_TIMEOUT;
+import static java.net.HttpURLConnection.HTTP_CONFLICT;
+import static java.net.HttpURLConnection.HTTP_ENTITY_TOO_LARGE;
+import static java.net.HttpURLConnection.HTTP_FORBIDDEN;
+import static java.net.HttpURLConnection.HTTP_GATEWAY_TIMEOUT;
+import static java.net.HttpURLConnection.HTTP_GONE;
+import static java.net.HttpURLConnection.HTTP_INTERNAL_ERROR;
+import static java.net.HttpURLConnection.HTTP_LENGTH_REQUIRED;
+import static java.net.HttpURLConnection.HTTP_NOT_ACCEPTABLE;
+import static java.net.HttpURLConnection.HTTP_NOT_FOUND;
+import static java.net.HttpURLConnection.HTTP_NOT_IMPLEMENTED;
 import static java.net.HttpURLConnection.HTTP_OK;
+import static java.net.HttpURLConnection.HTTP_PAYMENT_REQUIRED;
+import static java.net.HttpURLConnection.HTTP_PRECON_FAILED;
+import static java.net.HttpURLConnection.HTTP_PROXY_AUTH;
+import static java.net.HttpURLConnection.HTTP_REQ_TOO_LONG;
+import static java.net.HttpURLConnection.HTTP_UNAUTHORIZED;
+import static java.net.HttpURLConnection.HTTP_UNAVAILABLE;
+import static java.net.HttpURLConnection.HTTP_UNSUPPORTED_TYPE;
+import static java.net.HttpURLConnection.HTTP_VERSION;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -27,6 +58,7 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class FireboltAccountRetrieverTest {
+    private static final String GENERIC_ERROR_MESSAGE = "Server failed to execute query with the following error:";
 
 	@Spy
 	private final ObjectMapper objectMapper = new ObjectMapper();
@@ -43,8 +75,8 @@ class FireboltAccountRetrieverTest {
 
     @BeforeEach
     void setUp() {
-        fireboltGatewayUrlClient = new FireboltAccountRetriever<>(httpClient, objectMapper, fireboltConnection, null, null, "http://test-firebolt.io", "engineUrl", GatewayUrlResponse.class);
-        fireboltAccountIdResolver = new FireboltAccountRetriever<>(httpClient, objectMapper, fireboltConnection, null, null, "http://test-firebolt.io", "resolve", FireboltAccount.class);
+        fireboltGatewayUrlClient = new FireboltAccountRetriever<>(httpClient, objectMapper, fireboltConnection, null, null, "test-firebolt.io", "engineUrl", GatewayUrlResponse.class);
+        fireboltAccountIdResolver = new FireboltAccountRetriever<>(httpClient, objectMapper, fireboltConnection, null, null, "test-firebolt.io", "resolve", FireboltAccount.class);
     }
 
 	@Test
@@ -75,15 +107,53 @@ class FireboltAccountRetrieverTest {
         assertEquals("Failed to get engineUrl url for account acc", assertThrows(FireboltException.class, () -> fireboltGatewayUrlClient.retrieve("token", "acc")).getMessage());
     }
 
+    @ParameterizedTest
+    @CsvSource({
+            HTTP_BAD_REQUEST + "," + GENERIC_ERROR_MESSAGE,
+            HTTP_PAYMENT_REQUIRED + "," + GENERIC_ERROR_MESSAGE,
+            HTTP_FORBIDDEN + "," + GENERIC_ERROR_MESSAGE,
+            HTTP_BAD_METHOD + "," + GENERIC_ERROR_MESSAGE,
+            HTTP_NOT_ACCEPTABLE + "," + GENERIC_ERROR_MESSAGE,
+            HTTP_PROXY_AUTH + "," + GENERIC_ERROR_MESSAGE,
+            HTTP_CLIENT_TIMEOUT + "," + GENERIC_ERROR_MESSAGE,
+            HTTP_CONFLICT + "," + GENERIC_ERROR_MESSAGE,
+            HTTP_GONE + "," + GENERIC_ERROR_MESSAGE,
+            HTTP_LENGTH_REQUIRED + "," + GENERIC_ERROR_MESSAGE,
+            HTTP_PRECON_FAILED + "," + GENERIC_ERROR_MESSAGE,
+            HTTP_ENTITY_TOO_LARGE + "," + GENERIC_ERROR_MESSAGE,
+            HTTP_REQ_TOO_LONG + "," + GENERIC_ERROR_MESSAGE,
+            HTTP_UNSUPPORTED_TYPE + "," + GENERIC_ERROR_MESSAGE,
+            HTTP_INTERNAL_ERROR + "," + GENERIC_ERROR_MESSAGE,
+            HTTP_NOT_IMPLEMENTED + "," + GENERIC_ERROR_MESSAGE,
+            HTTP_BAD_GATEWAY + "," + GENERIC_ERROR_MESSAGE,
+            HTTP_GATEWAY_TIMEOUT + "," + GENERIC_ERROR_MESSAGE,
+            HTTP_VERSION + "," + GENERIC_ERROR_MESSAGE,
+
+            HTTP_NOT_FOUND + "," + "Account '%s' does not exist",
+            HTTP_UNAVAILABLE + "," + "Could not query Firebolt at https://test-firebolt.io/web/v3/account/%s/%s. The engine is not running.",
+            HTTP_UNAUTHORIZED + "," + "Could not query Firebolt at https://test-firebolt.io/web/v3/account/%s/%s. The operation is not authorized"
+    })
+    void testFailedAccountDataRetrieving(int statusCode, String errorMessageTemplate) throws IOException {
+        injectMockedResponse(httpClient, statusCode, null);
+        assertErrorMessage(fireboltAccountIdResolver, "one", format(errorMessageTemplate, "one", "resolve"));
+        assertErrorMessage(fireboltGatewayUrlClient, "two", format(errorMessageTemplate, "two", "engineUrl"));
+    }
+
+    private <T> void assertErrorMessage(FireboltAccountRetriever<T> accountRetriever, String accountName, String expectedErrorMessagePrefix) {
+       MatcherAssert.assertThat(Assert.assertThrows(FireboltException.class, () -> accountRetriever.retrieve("access_token", accountName)).getMessage(), Matchers.startsWith(expectedErrorMessagePrefix));
+    }
+
     private void injectMockedResponse(OkHttpClient httpClient, int code, Object payload) throws IOException {
         Response response = mock(Response.class);
         Call call = mock(Call.class);
         when(httpClient.newCall(any())).thenReturn(call);
         when(call.execute()).thenReturn(response);
-        ResponseBody body = mock(ResponseBody.class);
-        when(response.body()).thenReturn(body);
         when(response.code()).thenReturn(code);
         String gatewayResponse = new ObjectMapper().writeValueAsString(payload);
-        when(body.string()).thenReturn(gatewayResponse);
+        if (code == HTTP_OK) {
+            ResponseBody body = mock(ResponseBody.class);
+            when(response.body()).thenReturn(body);
+            when(body.string()).thenReturn(gatewayResponse);
+        }
     }
 }
