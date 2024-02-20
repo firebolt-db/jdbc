5388bb442da68aab3f18b82703f624beeff9e92b
alexradzin <alexander.radzin@firebolt.io>
2024-02-20 19:06:03 +0200

    version 3.0.2 release

--- src/test/java/com/firebolt/jdbc/util/VersionUtilTest.java
+++ src/test/java/com/firebolt/jdbc/util/VersionUtilTest.java
@@ -18,7 +18,7 @@ class VersionUtilTest {
 
 	@Test
 	void shouldGetProjectVersion() {
-		assertEquals("3.0.2-SNAPSHOT", VersionUtil.getDriverVersion());
+		assertEquals("3.0.2", VersionUtil.getDriverVersion());
 	}
 
 	@Test
