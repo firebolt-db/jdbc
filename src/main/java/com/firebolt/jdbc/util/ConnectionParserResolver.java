78d6f305220146e6a8e8b208bb8aa8cb7d622d16
ptiurin <petro.tiurin@firebolt.io>
2024-10-21 10:56:49 +0100

    Cleaner version

--- src/main/java/com/firebolt/jdbc/util/ConnectionParserResolver.java
+++ src/main/java/com/firebolt/jdbc/util/ConnectionParserResolver.java
@@ -4,22 +4,22 @@ import com.firebolt.jdbc.type.ParserVersion;
 import lombok.CustomLog;
 
 import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
 
 @CustomLog
 public class ConnectionParserResolver {
+    // Map of classes to parser versions
+    private static final Map<Class<?>, ParserVersion> PARSER_MAP = new HashMap<>(
+            Map.of(
+                    com.firebolt.jdbc.connection.FireboltConnectionUserPassword.class, ParserVersion.LEGACY,
+                    com.firebolt.jdbc.connection.FireboltConnectionServiceSecret.class, ParserVersion.CURRENT));
+
     private ConnectionParserResolver() {
         // Private constructor to prevent instantiation
     }
 
     public static ParserVersion getParserFromConnection(Connection connection) {
-        if (connection instanceof com.firebolt.jdbc.connection.FireboltConnectionUserPassword) {
-            return ParserVersion.LEGACY;
-        } else if (connection instanceof com.firebolt.jdbc.connection.FireboltConnectionServiceSecret) {
-            return ParserVersion.CURRENT;
-        } else {
-            log.warn("Connection instance is not recognized, assuming \"current\" sql string parser: "
-                    + connection.getClass().getName());
-            return ParserVersion.CURRENT;
-        }
+        return PARSER_MAP.getOrDefault(connection.getClass(), ParserVersion.CURRENT);
     }
 }
