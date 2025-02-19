4750fe13b3c8aff20986236efdd7ccc5d1c0e33b
Stepan Burlakov <stepansergeevitch@gmail.com>
2025-02-19 11:55:27 +0200

    feat: FIR-43038 allow jdbc to send batch as a single multi statement (#494)

--- src/integrationTest/resources/statements/prepared-statement-struct/ddl.sql
+++ src/integrationTest/resources/statements/prepared-statement-struct/ddl.sql
@@ -6,5 +6,5 @@ SET prevent_create_on_information_schema=true;
 SET enable_create_table_with_struct_type=true;
 DROP TABLE IF EXISTS test_struct;
 DROP TABLE IF EXISTS test_struct_helper;
-CREATE TABLE IF NOT EXISTS test_struct(id int not null, s struct(a array(text) not null, "b column" datetime null) not null);
+CREATE TABLE IF NOT EXISTS test_struct(id int not null, s struct(a array(text) null, "b column" datetime null) not null);
 CREATE TABLE IF NOT EXISTS test_struct_helper(a array(text) not null, "b column" datetime null);
