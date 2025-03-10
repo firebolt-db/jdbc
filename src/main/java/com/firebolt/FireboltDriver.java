e9b485764344c9beb13583ba66122f07c5b2ede8
Petro Tiurin <93913847+ptiurin@users.noreply.github.com>
2025-03-10 11:42:44 +0200

    Update FireboltDriver.java

--- src/main/java/com/firebolt/FireboltDriver.java
+++ src/main/java/com/firebolt/FireboltDriver.java
@@ -52,6 +52,21 @@ public class FireboltDriver implements Driver {
 	public int getMinorVersion() {
 		return VersionUtil.getDriverMinorVersion();
 	}
+	
+	@Override
+	public int getMinorVersion() {
+		return VersionUtil.getDriverMinorVersion();
+	}
+	
+	
+	public int getMinorVersion1() {
+		return VersionUtil.getDriverMinorVersion();
+	}
+	
+	
+	public int getMinorVersion2() {
+		return VersionUtil.getDriverMinorVersion();
+	}
 
 	@Override
 	public boolean jdbcCompliant() {
