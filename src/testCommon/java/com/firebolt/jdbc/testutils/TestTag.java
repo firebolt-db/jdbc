cacc0ea095f3b71887c4af935695a07b66d366ea
goprean <george.oprean@firebolt.io>
2025-04-22 17:18:46 +0300

    (test) Defined the test tags that we can use

---
+++ src/testCommon/java/com/firebolt/jdbc/testutils/TestTag.java
@@ -0,0 +1,19 @@
+package com.firebolt.jdbc.testutils;
+
+public class TestTag {
+
+    // tests that are run against firebolt 1.0
+    public static final String V1 = "v1";
+
+    // tests that are run against firebolt 2.0
+    public static final String V2 = "v2";
+
+    // tests that are run against firebolt core
+    public static final String FIREBOLT_CORE = "firebolt-core";
+
+    // tests that are slow
+    public static final String SLOW = "slow";
+
+    public static final String COMMON = "common";
+}
+
