45791c07f40102612a6e268ffc7b5415c878afd2
goprean <45954629+goprean@users.noreply.github.com>
2025-05-12 19:21:58 +0300

    FIR-44420 added the support firebolt core (#570)

---
+++ src/main/java/com/firebolt/jdbc/FireboltBackendType.java
@@ -0,0 +1,40 @@
+package com.firebolt.jdbc;
+
+import lombok.Getter;
+
+/**
+ * What type of backend is driver connecting to
+ */
+@Getter
+public enum FireboltBackendType {
+
+    /**
+     * firebolt hosted cloud version 1.0
+     */
+    CLOUD_1_0("cloudv1"),
+
+    /**
+     * firebolt hosted cloud version 2.0
+     */
+    CLOUD_2_0("cloud"),
+
+    /**
+     * only used for testing by packdb
+     */
+    DEV("dev"),
+
+    /**
+     * Connecting to firebolt core
+     */
+    FIREBOLT_CORE("core");
+
+    /**
+     * This would be the value that would be present on the connection string
+     */
+    private String value;
+
+    FireboltBackendType(String value) {
+        this.value = value;
+    }
+
+}
