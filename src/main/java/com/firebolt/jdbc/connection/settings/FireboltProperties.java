6df1e8f4ed37da050a63085b95f8a6cce582c38d
Bogdan Truta <bogdan.truta@firebolt.io>
2025-08-14 14:07:31 +0300

    fix addProperty parameter value

--- src/main/java/com/firebolt/jdbc/connection/settings/FireboltProperties.java
+++ src/main/java/com/firebolt/jdbc/connection/settings/FireboltProperties.java
@@ -289,7 +289,7 @@ public class FireboltProperties {
 	}
 
 	public void addProperty(Entry<String, String> property) {
-		addProperty(property.getKey(), property.getValue(), false);
+		addProperty(property.getKey(), property.getValue(), true);
 	}
 
 	public String getHttpConnectionUrl() {
@@ -307,7 +307,7 @@ public class FireboltProperties {
 		String engineHost = engineUrl[0].replaceFirst("^https?://", ""); // just in case remove URL scheme although right now server never returns it
 		String[] engineQuery = engineUrl.length > 1 ? engineUrl[1].split("&") : new String[0];
 		// get properties from query string and update values
-		Arrays.stream(engineQuery).map(prop -> prop.split("=")).filter(a -> a.length == 2).forEach(prop -> addProperty(prop[0], prop[1], false));
+		Arrays.stream(engineQuery).map(prop -> prop.split("=")).filter(a -> a.length == 2).forEach(prop -> addProperty(prop[0], prop[1], true));
 		return engineHost;
 	}
 }
