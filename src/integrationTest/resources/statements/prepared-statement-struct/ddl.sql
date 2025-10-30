781afae2aee13c13db129f7740680d804d87ada9
Petro Tiurin <93913847+ptiurin@users.noreply.github.com>
2025-10-30 09:51:00 +0000

    chore(NoTicket): remove unnecessary setting for create table test (#644)

--- src/integrationTest/resources/statements/prepared-statement-struct/ddl.sql
+++ src/integrationTest/resources/statements/prepared-statement-struct/ddl.sql
@@ -2,7 +2,6 @@ SET advanced_mode=1;
 SET enable_create_table_v2=true;
 SET enable_struct_syntax=true;
 SET prevent_create_on_information_schema=true;
-SET enable_create_table_with_struct_type=true;
 DROP TABLE IF EXISTS test_struct;
 DROP TABLE IF EXISTS test_struct_helper;
 CREATE TABLE IF NOT EXISTS test_struct(id int not null, s struct(a array(text) null, "b column" datetime null) not null);
