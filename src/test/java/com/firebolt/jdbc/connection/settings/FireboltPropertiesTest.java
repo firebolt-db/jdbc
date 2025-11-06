7a2ce554591d7cfe529ba65461d090d19a04168a
goprean <45954629+goprean@users.noreply.github.com>
2025-11-06 10:43:38 +0200

    feat: make the compress_request_payload false by default FIR-50520 (#652)

--- src/test/java/com/firebolt/jdbc/connection/settings/FireboltPropertiesTest.java
+++ src/test/java/com/firebolt/jdbc/connection/settings/FireboltPropertiesTest.java
@@ -25,7 +25,7 @@ class FireboltPropertiesTest {
 				.keepAliveTimeoutMillis(300000).maxConnectionsTotal(300).maxRetries(3)
 				.socketTimeoutMillis(0).connectionTimeoutMillis(60000).tcpKeepInterval(30).environment("app").tcpKeepIdle(60)
 				.tcpKeepCount(10).connectionCachingEnabled(true).preparedStatementParamStyle("native")
-				.compressRequestPayload(true)
+				.compressRequestPayload(false)
 				.build();
 
 		Properties properties = new Properties();
@@ -63,7 +63,7 @@ class FireboltPropertiesTest {
 				.maxConnectionsTotal(300).maxRetries(3).socketTimeoutMillis(20).connectionTimeoutMillis(60000)
 				.tcpKeepInterval(30).tcpKeepIdle(60).tcpKeepCount(10).environment("app").validateOnSystemEngine(true)
 				.mergePreparedStatementBatches(true).connectionCachingEnabled(true).preparedStatementParamStyle("fb_numeric")
-				.compressRequestPayload(true)
+				.compressRequestPayload(false)
 				.build();
 		assertEquals(expectedDefaultProperties, new FireboltProperties(properties));
 	}
